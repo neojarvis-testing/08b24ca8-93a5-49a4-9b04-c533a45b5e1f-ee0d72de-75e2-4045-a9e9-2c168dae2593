88a5697d4d069cfc77922f3aa698181382e90a5e
deepika-97 <sit20it034@sairamtap.edu.in>
2025-04-12 07:14:21 +0000

    models with  changes

--- dotnetapp/Models/Feedback.cs
+++ dotnetapp/Models/Feedback.cs
@@ -7,6 +7,35 @@ namespace dotnetapp.Models
 {
     public class Feedback
     {
+        public int FeedbackId{get;set;}
+        public int UserId{get;set;}
+        public string Comments{get;set;}
+        public DateTime DateProvided{get;set;}
+        public User? User{get;set;}
         
     }
+}
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Feedback
+{
+    [Key]
+    public int FeedbackId { get; set; }
+
+    [Required]
+
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(1000, ErrorMessage = "Comments cannot exceed 1000 characters.")]
+    public string Comments { get; set; }
+
+    [Required]
+    public DateTime DateProvided { get; set; }
+
+    // Navigation property
+    public  User? User { get; set; }
 }
