3df77ea85fd3d36452ffa9cb7cb7d953cb3330c9
deepika-97 <sit20it034@sairamtap.edu.in>
2025-04-14 07:33:20 +0000

    navbar with changes

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsernavComponent } from './components/usernav/usernav.component';
+import { ManagernavComponent } from './components/managernav/managernav.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UseraddfeedbackComponent
+    UseraddfeedbackComponent,
+    HomeComponent,
+    UsernavComponent,
+    ManagernavComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
