391953e6284cf00c014a1c966d5e311df054f2c4
san-2000 <sayhisanthosh@gmail.com>
2025-04-14 09:31:12 +0000

    Authentication done with tc

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,39 +1,86 @@
 using Microsoft.EntityFrameworkCore;
 using dotnetapp.Data;
 using dotnetapp.Services;
- 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
- 
+
 // Add services to the container.
- 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(db =>
+builder.Services.AddSwaggerGen(c =>
 {
-    db.UseSqlServer(builder.Configuration.GetConnectionString("Myconnstring"));
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT Auth API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by your token"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
- 
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Myconnstring"))
+);
+
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ISavingsPlanService, SavingsPlanService>();
 builder.Services.AddScoped<IPlanApplicationService, PlanApplicationService>();
 builder.Services.AddScoped<IFeedbackService, FeedbackService>();
- 
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
- 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
 app.UseHttpsRedirection();
- 
+
+app.UseAuthentication();
 app.UseAuthorization();
- 
+
 app.MapControllers();
- 
+
 app.Run();
- 
