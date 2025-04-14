90290db793f3fe2d1ba3f2623e4f301009074389
kakkarot0 <arun00gound@gmail.com>
2025-04-14 04:49:55 +0000

    Created a useraddfeedback file

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UseraddfeedbackComponent
   ],
   imports: [
     BrowserModule,
