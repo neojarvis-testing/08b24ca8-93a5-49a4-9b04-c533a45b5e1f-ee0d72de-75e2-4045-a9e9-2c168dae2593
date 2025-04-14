5fe050556f751864ede62216f87eda9a2d21e5e2
san-2000 <sayhisanthosh@gmail.com>
2025-04-14 11:23:34 +0000

    commiting

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { UsernavComponent } from './components/usernav/usernav.component';
 import { UserplanapplicationformComponent } from './components/userplanapplicationform/userplanapplicationform.component';
 import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
-
+import { PiggyBankLoaderComponent } from './components/piggy-bank-loader/piggy-bank-loader.component';
 
 
 @NgModule({
@@ -33,7 +33,8 @@ import { UserviewfeedbackComponent } from './components/userviewfeedback/uservie
     UseraddfeedbackComponent,
     UsernavComponent,
     UserplanapplicationformComponent,
-    UserviewfeedbackComponent
+    UserviewfeedbackComponent,
+    PiggyBankLoaderComponent
 
   ],
   imports: [
