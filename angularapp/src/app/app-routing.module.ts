87ca4afc5c7c126dc0c34cd6fd630af49305bbd0
deepika-97 <sit20it034@sairamtap.edu.in>
2025-04-14 11:19:47 +0000

    navbarsssssss

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsernavComponent } from './components/usernav/usernav.component';
 import { ManagernavComponent } from './components/managernav/managernav.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'',component:UsernavComponent},
+  {path:'',component:ManagernavComponent}
+  
 ];
 
 @NgModule({
