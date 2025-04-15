088dd3bddffd5a08787e0a70d85dafdaea40b964
deepika-97 <sit20it034@sairamtap.edu.in>
2025-04-15 04:05:00 +0000

    error resolving

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,22 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { HomeComponent } from './components/home/home.component';
-// import { UsernavComponent } from './components/usernav/usernav.component';
-// import { ManagernavComponent } from './components/managernav/managernav.component';
-// import { ManagereditsavingsplanComponent } from './components/managereditsavingsplan/managereditsavingsplan.component';
-
-// const routes: Routes = [
-//   {path: 'editsavingsplan', component:ManagereditsavingsplanComponent},
-  
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
-
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -24,6 +5,7 @@ import { UsernavComponent } from './components/usernav/usernav.component';
 import { ManagernavComponent } from './components/managernav/managernav.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserviewsavingsplanComponent } from './components/userviewsavingsplan/userviewsavingsplan.component';
+import { ManagereditsavingsplanComponent } from './components/managereditsavingsplan/managereditsavingsplan.component';
 
 const routes: Routes = [
   //{path:'',component:HomeComponent},
@@ -31,13 +13,9 @@ const routes: Routes = [
   //{path:'',component:UsernavComponent},
   //{path:'',component:ManagernavComponent},
  // {path:'',component:UserviewsavingsplanComponent}
-  
-import { ManagereditsavingsplanComponent } from './components/managereditsavingsplan/managereditsavingsplan.component';
 
-const routes: Routes = [
-  { path: 'editsavingsplan/:savingsPlanId', component: ManagereditsavingsplanComponent },
-  // other routes
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
