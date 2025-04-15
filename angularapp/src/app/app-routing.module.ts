fbd3eb1351976ef3663dcfbc42114ad97a80d2d2
kakkarot0 <arun00gound@gmail.com>
2025-04-15 04:25:08 +0000

    completed userfeedback Completed

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsernavComponent } from './components/usernav/usernav.component';
 import { ManagernavComponent } from './components/managernav/managernav.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { ManagerviewfeedbackComponent } from './components/managerviewfeedback/managerviewfeedback.component';
 
 const routes: Routes = [
+ 
 ];
 
 @NgModule({
