42e7d42618e6109297d69c647765cf040011421a
chetankhandre <chetankhandre007@gmail.com>
2025-04-16 07:28:09 +0000

    manager view

--- angularapp/src/app/models/planapplication.model.ts
+++ angularapp/src/app/models/planapplication.model.ts
@@ -3,11 +3,11 @@ import { SavingsPlan } from "./savingsplan.model";
 export interface PlanApplication{
     PlanApplicationId?:number;
     AppliedAmount:number;
-    Status:string;
+    status:string;
     ApplicationDate:string;
     Remarks:string;
     ProofDocument:string;
     UserId:number;
     SavingsPlanId:number;
-    SavingsPlan?: SavingsPlan
+    SavingsPlan?: SavingsPlan;
 }
