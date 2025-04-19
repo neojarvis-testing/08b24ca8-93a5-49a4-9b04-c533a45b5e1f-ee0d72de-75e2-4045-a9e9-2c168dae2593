a0c714a52467d867a79ccd4dd598a9ca5a96a8db
anmolpathakkk202002 <anmolpathak44448@gmail.com>
2025-04-19 11:35:16 +0000

    17.05 Commit

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  // apiUrl:'https://8080-daabfcebbfcbadecdecaaeaadadfeeddeeaecdae.premiumproject.examly.io/api'
   apiUrl:'https://8080-daabfcebbfcbadecdecaaeaadadfeeddeeaecdae.premiumproject.examly.io/api'
 };
  
